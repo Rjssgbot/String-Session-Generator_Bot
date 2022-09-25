4c7a0c3be6dd4c4603cd5fd69f0c9f3f19bf3edb
Rjssgbot <114350616+Rjssgbot@users.noreply.github.com>
2022-09-25 20:54:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 class Config:
-    API_ID = int(os.environ.get('API_ID', 0))
-    API_HASH = str(os.environ.get('API_HASH', None))
-    BOT_TOKEN = str(os.environ.get('BOT_TOKEN', None))
-    MONGO_URI = str(os.environ.get('MONGO_URI', None))
-    UPDATES_CHANNEL = str(os.environ.get('UPDATES_CHANNEL', None)) #Start Without @
+    API_ID = int(os.environ.get('API_ID', "19031598"))
+    API_HASH = str(os.environ.get('API_HASH', "14e2d2af6e6dc769e60991cab81027d5"))
+    BOT_TOKEN = str(os.environ.get('BOT_TOKEN', "5665710621:AAFP-O8AW3ekB_basaPR3A7w3YpQDMQbCDY"))
+    MONGO_URI = str(os.environ.get('MONGO_URI', "mongodb+srv://Oyehoye143:Oyehoye143@cluster0.eth7a3p.mongodb.net/?retryWrites=true&w=majority"))
+    UPDATES_CHANNEL = str(os.environ.get('UPDATES_CHANNEL', "RomeoBot_OP")) #Start Without @
