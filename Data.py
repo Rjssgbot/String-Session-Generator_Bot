4f921cb41b4f75331aefe4309985442d84e91bc3
Romeo-RJ <110713338+Romeo-RJ@users.noreply.github.com>
2022-09-25 20:20:43 +0530

    Create Data.py

---
+++ Data.py
@@ -0,0 +1,59 @@
+from pyrogram.types import InlineKeyboardButton
+
+
+class Data:
+    # Start Message
+    START = """
+Hey {}
+
+Welcome to {}
+
+I can generate pyrogram and telethon string session . 
+Use the below button and go ahead !
+
+By @Rjssgbot
+    """
+
+    # Home Button
+    home_buttons = [
+        [InlineKeyboardButton("🔷 Start Generating Session 🔷", callback_data="generate")],
+        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")]
+    ]
+
+    generate_button = [
+        [InlineKeyboardButton("🔷 Start Generating Session 🔷", callback_data="generate")]
+    ]
+
+    # Rest Buttons
+    buttons = [
+        [InlineKeyboardButton("🔷 Start Generating Session 🔷", callback_data="generate")],
+        [
+            InlineKeyboardButton("How to Use ❔", callback_data="help"),
+            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+        ],
+    ]
+
+    # Help Message
+    HELP = """
+✨ **Available Commands** ✨
+
+/about - About The Bot
+/help - To display current Message
+/start - Start the Bot
+/generate - Generate String Session
+/cancel - Cancel the process
+/restart - Cancel the process
+"""
+
+    # About Message
+    ABOUT = """
+**About This Bot** 
+
+A telegram bot to generate pyrogram and telethon string session by @Rjssgbot
+
+Framework : [Pyrogram](docs.pyrogram.org)
+
+Language : [Python](www.python.org)
+
+Developer : 𝕾🅡︎🅐︎🅙︎𝕾
+    """
